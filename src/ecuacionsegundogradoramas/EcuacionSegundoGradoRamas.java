e3fea86e6352af418ac9c3fe6c04b2bc454ddc38
Manuelvik <U22303632@utp.edu.pe>
2026-04-28 21:50:11 -0500

    Mejora 3: soporte para coeficientes decimales

--- src/ecuacionsegundogradoramas/EcuacionSegundoGradoRamas.java
+++ src/ecuacionsegundogradoramas/EcuacionSegundoGradoRamas.java
@@ -6,17 +6,17 @@ public class EcuacionSegundoGradoRamas {
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in); 
  
-        int a, b, c; 
+        double a, b, c; 
         double discriminante, x1, x2; 
  
         System.out.print("Ingrese a: "); 
-        a = sc.nextInt(); 
+        a = sc.nextDouble(); 
  
         System.out.print("Ingrese b: "); 
-        b = sc.nextInt(); 
+        b = sc.nextDouble(); 
  
         System.out.print("Ingrese c: "); 
-        c = sc.nextInt(); 
+        c = sc.nextDouble(); 
  
         if (a == 0) { 
             System.out.println("No es una ecuacion de segundo grado."); 
@@ -30,14 +30,14 @@ public class EcuacionSegundoGradoRamas {
             x2 = (-b - Math.sqrt(discriminante)) / (2.0 * a); 
  
             System.out.println("Dos raices reales diferentes:"); 
-            System.out.println("Raiz 1: " + x1); 
-            System.out.println("Raiz 2: " + x2); 
-        } else if (discriminante == 0) { 
-            x1 = (-b) / (2.0 * a); 
-            System.out.println("Una raiz real doble:"); 
-            System.out.println("Raiz: " + x1); 
-        } else { 
-            System.out.println("No existen raices reales."); 
-        } 
-    } 
+System.out.println("Raiz 1: " + x1); 
+System.out.println("Raiz 2: " + x2); 
+} else if (discriminante == 0) { 
+x1 = (-b) / (2.0 * a); 
+System.out.println("Una raiz real doble:"); 
+System.out.println("Raiz: " + x1); 
+} else { 
+System.out.println("No existen raices reales."); 
 } 
+} 
+}
